705557c7644f756be17f27b55f1c52822e13bce3
YixinWu1992 <37315357+YixinWu1992@users.noreply.github.com>
2018-04-21 11:11:54 -0400

    Add files via upload

--- Spam/GetAllEmailPath.py
+++ Spam/GetAllEmailPath.py
@@ -14,19 +14,19 @@ def load_all_data():
     lines_3 = fd_3.readlines()
     while lines_1 or lines_2 or lines_3:
         if lines_1:
-            line_1 = lines_1.pop(0).strip(" \n")
+            line_1 = lines_1.pop(0)
             line_1_tokens = line_1.split(" ")
-            line_1_path = line_1_tokens[0] + " trec/trec05p-1" + line_1_tokens[1][2:] + "\n"
+            line_1_path = line_1_tokens[0] + " trec/trec05p-1" + line_1_tokens[1][2:]
             fd.write(line_1_path)
         if lines_2:
-            line_2 = lines_2.pop(0).strip(" \n")
+            line_2 = lines_2.pop(0)
             line_2_tokens = line_2.split(" ")
-            line_2_path = line_2_tokens[0] + " trec/trec06p" + line_2_tokens[1][2:] + "\n"
+            line_2_path = line_2_tokens[0] + " trec/trec06p" + line_2_tokens[1][2:]
             fd.write(line_2_path)
         if lines_3:
-            line_3 = lines_3.pop(0).strip(" \n")
+            line_3 = lines_3.pop(0)
             line_3_tokens = line_3.split(" ")
-            line_3_path = line_3_tokens[0] + " trec/trec07p" + line_3_tokens[1][2:] + "\n"
+            line_3_path = line_3_tokens[0] + " trec/trec07p" + line_3_tokens[1][2:]
             fd.write(line_3_path)
     fd_1.close()
     fd_2.close()
@@ -44,9 +44,46 @@ def create_3000():
         fd.write(line)
     fd.close()
 
+def create_3000_even():
+    path_1 = "trec/trec05p-1/full/index"
+    path_2 = "trec/trec06p/full/index"
+    path_3 = "trec/trec07p/full/index"
+    target_path = "trec/train/random3000even/index"
+    fd_1 = open(path_1, "r")
+    fd_2 = open(path_2, "r")
+    fd_3 = open(path_3, "r")
+    fd = open(target_path, "w")
+    lines_1 = fd_1.readlines()
+    lines_2 = fd_2.readlines()
+    lines_3 = fd_3.readlines()
+    random_lines_1 = random.sample(lines_1, 1000)
+    random_lines_2 = random.sample(lines_2, 1000)
+    random_lines_3 = random.sample(lines_3, 1000)
+    while random_lines_1 or random_lines_2 or random_lines_3:
+        if random_lines_1:
+            random_line_1 = random_lines_1.pop(0)
+            random_line_1_tokens = random_line_1.split(" ")
+            random_line_1_path = random_line_1_tokens[0] + " trec/trec05p-1" + random_line_1_tokens[1][2:]
+            fd.write(random_line_1_path)
+        if random_lines_2:
+            random_line_2 = random_lines_2.pop(0)
+            random_line_2_tokens = random_line_2.split(" ")
+            random_line_2_path = random_line_2_tokens[0] + " trec/trec06p" + random_line_2_tokens[1][2:]
+            fd.write(random_line_2_path)
+        if random_lines_3:
+            random_line_3 = random_lines_3.pop(0)
+            random_line_3_tokens = random_line_3.split(" ")
+            random_line_3_path = random_line_3_tokens[0] + " trec/trec07p" + random_line_3_tokens[1][2:]
+            fd.write(random_line_3_path)
+    fd_1.close()
+    fd_2.close()
+    fd_3.close()
+    fd.close()
+
 def main():
     #load_all_data()
-    create_3000()
+    #create_3000()
+    create_3000_even()
 
 if __name__ == "__main__":
     main()
