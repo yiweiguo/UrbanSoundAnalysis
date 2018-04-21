f9ac9ea352ce93df12e8ffe8e667636364ffc3df
YixinWu1992 <37315357+YixinWu1992@users.noreply.github.com>
2018-04-21 08:40:38 -0400

    Add files via upload

--- Spam/FeatureLoading.py
+++ Spam/FeatureLoading.py
@@ -2,10 +2,14 @@ from collections import defaultdict
 import email
 import math
 import os
+import tensorflow as tf
+import numpy as np
+import operator
+import pandas
 
 
 class Features:
-    def __init__(self, tag_dir):
+    def __init__(self, tag_dir, is_naive_bayes=True):
         self.index_path = tag_dir
         self.sm_ham = 0.00000001
         self.sm_spam = 0.00000001
@@ -27,6 +31,13 @@ class Features:
         self.count_spam = 0
         self.p_ham = 0.0
         self.p_spam = 0.0
+        self.feature_words = []
+        #self.database = None
+        self.feature_vector = None
+        self.label_vector = None
+        self.regularization_param = 0.001
+        self.is_naive_bayes = is_naive_bayes
+
 
     def taglize(self):
         index_f = open(self.index_path, 'r')
@@ -64,8 +75,10 @@ class Features:
                             self.v_uniham[word] += 1
                             self.c_ham[len(word)] += 1
                             if i != sum_len - 1:
-                                bigram = tuple(
-                                    [tokens[0][i], tokens[0][i + 1]])
+                                if(self.is_naive_bayes):
+                                    bigram = tuple([tokens[0][i], tokens[0][i + 1]])
+                                else:
+                                    bigram = tokens[0][i] + " " + tokens[0][i + 1]
                                 self.v_biham[bigram] += 1
                     else:
                         self.num_spam += 1
@@ -76,9 +89,124 @@ class Features:
                             self.v_unispam[word] += 1
                             self.c_spam[len(word)] += 1
                             if i != sum_len - 1:
-                                bigram = tuple(
-                                    [tokens[0][i], tokens[0][i + 1]])
+                                if (self.is_naive_bayes):
+                                    bigram = tuple([tokens[0][i], tokens[0][i + 1]])
+                                else:
+                                    bigram = tokens[0][i] + " " + tokens[0][i + 1]
                                 self.v_bispam[bigram] += 1
+        if(not self.is_naive_bayes):
+            feature_number = 1000
+            self.feature_words = self.mutual_information(feature_number)
+            feature_columns, label_vector, size = self.get_train_set(head, corpus)
+            #print(feature_columns.keys())
+            self.feature_vector = pandas.DataFrame(data=feature_columns, dtype=np.int32)
+            #print(self.feature_vector)
+            self.label_vector = pandas.Series(data=label_vector, dtype=np.int32)
+
+
+    def get_train_set(self, head, corpus):
+        label_vector = np.array([])
+        #feature_column = dict.fromkeys(self.feature_words, np.array([]))
+        feature_column = {}
+        num = 0
+        for word in self.feature_words:
+            key = "String" + str(num)
+            num += 1
+            feature_column[key] = np.array([])
+        counter = 0
+        for (tag, email) in corpus:
+            if os.path.exists(head + email):
+                tokens = self.tokenlize(head + email)
+                if tokens[1]:
+                    if tag.lower() == 'ham':
+                        label_vector = np.hstack([label_vector, 0])
+                    else:
+                        label_vector = np.hstack([label_vector, 1])
+                    sum_len = len(tokens[0])
+                    tmp_dict = defaultdict(int)
+                    for i in range(sum_len):
+                        word = tokens[0][i]
+                        tmp_dict[word] += 1
+                        if i != sum_len - 1:
+                            bigram = tokens[0][i] + " " + tokens[0][i + 1]
+                            tmp_dict[bigram] += 1
+                    #print(tmp_dict)
+                    for key in feature_column:
+                        feature_column[key] = np.hstack([feature_column[key], 0])
+                    for key in tmp_dict:
+                        if key in self.feature_words:
+                            ind = self.feature_words.index(key)
+                            key_string = "String" + str(ind)
+                            feature_column[key_string][counter] = tmp_dict[key]
+                    counter += 1
+        #print(feature_column)
+        #print(label_vector)
+        #print(counter)
+        return feature_column, label_vector, counter
+
+
+    def mutual_information(self, word_number):
+        p_ham = float(self.num_ham / (self.num_ham + self.num_spam))
+        p_spam = 1.0 - p_ham
+        #num_words_and_phrases = self.count_ham + self.count_spam
+        words_and_phrases = defaultdict(int)
+        num_words_and_phrases = 0
+        num_words_and_phrases_ham = 0
+        num_words_and_phrases_spam = 0
+        mutual_information_value = {}
+        for key in self.v_uniham:
+            #key_string = "String: " + key
+            key_string = key
+            words_and_phrases[key_string] += self.v_uniham[key]
+            num_words_and_phrases += self.v_uniham[key]
+            num_words_and_phrases_ham += self.v_uniham[key]
+        for key in self.v_unispam:
+            #key_string = "String: " + key
+            key_string = key
+            words_and_phrases[key_string] += self.v_unispam[key]
+            num_words_and_phrases += self.v_unispam[key]
+            num_words_and_phrases_spam += self.v_unispam[key]
+        for key in self.v_biham:
+            #key_string = "String: " + key
+            key_string = key
+            words_and_phrases[key_string] += self.v_biham[key]
+            num_words_and_phrases += self.v_biham[key]
+            num_words_and_phrases_ham += self.v_biham[key]
+        for key in self.v_bispam:
+            #key_string = "String: " + key
+            key_string = key
+            words_and_phrases[key_string] += self.v_bispam[key]
+            num_words_and_phrases += self.v_bispam[key]
+            num_words_and_phrases_spam += self.v_bispam[key]
+
+        for token in words_and_phrases:
+            p_word = float(words_and_phrases[token] / num_words_and_phrases)
+            p_ham_joint = float((self.v_uniham[token] + self.v_biham[token]) / num_words_and_phrases_ham) * p_ham
+            p_spam_joint = float((self.v_unispam[token] + self.v_bispam[token]) / num_words_and_phrases_spam) * p_spam
+            '''print(token)
+            print(p_ham_joint)
+            print(p_word)
+            print(p_ham)
+            print(p_spam_joint)
+            print(p_spam)
+            print(self.v_uniham[token])
+            print(self.v_biham[token])
+            print(self.v_unispam[token])
+            print(self.v_bispam[token])
+            print(num_words_and_phrases_ham)'''
+            try:
+                mutual_information_value[token] = p_ham_joint * math.log(p_ham_joint / (p_word * p_ham), 2) + p_spam_joint * math.log(p_spam_joint / (p_word * p_spam), 2)
+            except ValueError:
+                if(p_spam_joint == 0 or p_ham_joint == 0):
+                    mutual_information_value[token] = float("-Inf")
+                else:
+                    mutual_information_value[token] = float("Inf")
+
+        sorted_mutual_info_value = sorted(mutual_information_value.items(), key=operator.itemgetter(1), reverse=True)
+        #print(sorted_mutual_info_value[0])
+        return [sorted_mutual_info_value[ind][0] for ind in range(0, word_number)]
+
+
 
     def train_log_prob(self):
         self.p_ham = float(self.num_ham / (self.num_ham + self.num_spam))
@@ -92,6 +220,8 @@ class Features:
             self.prob_spam[word] = math.log((self.v_unispam[word] + self.sm_spam) / (
                 self.count_spam + self.sm_spam * (len(self.v_unispam) + 1)))
         for bigram in self.v_biham.keys():
+            #print(self.v_biham[bigram])
+            #print(self.v_uniham[bigram[0]])
             self.prob_biham[bigram] = math.log(
                 self.v_biham[bigram] * 1.0 / self.v_uniham[bigram[0]])
         for bigram in self.v_bispam.keys():
---
+++ Spam/NeuralNetwork.py
@@ -0,0 +1,103 @@
+from collections import defaultdict
+import email
+import math
+import os
+import numpy as np
+from FeatureLoading import Features
+import tensorflow as tf
+import pandas
+
+class NeuralNetwork(object):
+    def __init__(self, features, classifier):
+        self.features = features
+        self.classifier = classifier
+
+    def testlize(self, test_dir):
+        index_f = open(test_dir, 'r')
+        corpus = [tuple((line.split('\n'))[0].split(' ..'))
+                  for line in index_f]
+        index_f.close()
+        return corpus
+
+    def eval_input_fn(self, feature_vectors, feature_labels):
+        dataset = tf.data.Dataset.from_tensor_slices((dict(feature_vectors), feature_labels)).batch(1000)
+        return dataset
+
+    def classification(self, super_path, corpus):
+        test_class_vector = np.array([])
+        test_feature_columns = {}
+        for ind in range(0, len(self.features.feature_words)):
+            key_string = "String" + str(ind)
+            test_feature_columns[key_string] = np.array([])
+        counter = 0
+        for label, path in corpus:
+            for key in test_feature_columns:
+                test_feature_columns[key] = np.hstack([test_feature_columns[key], 0])
+            if label.lower() == "spam":
+                test_class_vector = np.hstack([test_class_vector, 1])
+            else:
+                test_class_vector = np.hstack([test_class_vector, 0])
+            email_path = super_path + path
+            if os.path.exists(email_path):
+                tokens = self.features.tokenlize(email_path)
+                if tokens[1]:
+                    words = tokens[0]
+                    for i in range(len(words)):
+                        word = words[i]
+                        if word in self.features.feature_words:
+                            index = self.features.feature_words.index(word)
+                            key_string = "String" + str(index)
+                            test_feature_columns[key_string][counter] += 1
+                            if i != len(words) - 1:
+                                bigram = tokens[0][i] + " " + tokens[0][i + 1]
+                                if bigram in self.features.feature_words:
+                                    index = self.features.feature_words.index(bigram)
+                                    key_string = "String" + str(index)
+                                    test_feature_columns[key_string][counter] += 1
+            counter += 1
+            '''
+        for key in test_feature_columns:
+            test_feature_columns[key] = tf.constant(test_feature_columns[key], tf.int64)
+        test_class_vector = tf.constant(test_class_vector, tf.int64)'''
+        test_feature_columns = pandas.DataFrame(data=test_feature_columns, dtype=np.int32)
+        test_class_vector = pandas.Series(data=test_class_vector, dtype=np.int32)
+        result = self.classifier.evaluate(
+            input_fn=lambda : self.eval_input_fn(test_feature_columns, test_class_vector))
+        print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**result))
+
+
+
+
+def DNN_input_fn(features, labels, batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    #print(final_database)
+    return dataset
+
+def main():
+    head = 'trec/trec07p'
+    level = 'train'
+    features = Features(head + '/' + level + '/index', is_naive_bayes=False)
+    features.prepare(head, features.taglize())
+    columns = []
+    for ind in range(0, len(features.feature_words)):
+        key_string = "String" + str(ind)
+        columns.append(tf.feature_column.numeric_column(key=key_string))
+    classifier = tf.estimator.DNNClassifier(feature_columns=columns,
+                                                 hidden_units=[10, 10], n_classes=2)
+    #print(len(columns))
+    #print(features.label_vector)
+    classifier.train(input_fn=lambda:DNN_input_fn(features.feature_vector, features.label_vector, 100), steps=1000)
+    nnClassifier = NeuralNetwork(features, classifier)
+    while True:
+        test_head = input('Input test head:')
+        if test_head == 'q':
+            return
+        test_level = input('Input test level:')
+        print('Testing...\n')
+        cases = nnClassifier.testlize(test_head + '/' + test_level + '/index')
+        nnClassifier.classification(test_head, cases)
+
+
+if __name__ == '__main__':
+    main()
